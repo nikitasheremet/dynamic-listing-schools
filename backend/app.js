5d899a671339e2fc9e9be7a4103a5aaa810b6d15
Nikita Sheremet <nikita.sheremet253@gmail.com>
2019-12-11 15:44:52 -0500

    connect to pg database and create query for all rows from schools table under / route

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,29 @@
 const express = require("express");
 let app = express();
-const port = 3000;
+const port = 4000;
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "dynalist",
+  password: "password",
+  host: "localhost",
+  database: "dynamic_list"
+});
+
+// module.exports = {
+//   query: (text, params, callback) => {
+//     return pool.query(text, params, callback)
+//   },
+// }
+
+app.get("/", (req, res) => {
+  pool.query(`SELECT * FROM schools;`).then(result => {
+    let rows = result.rows[0];
+    res.status(200).json({ rows });
+  });
+});
+app.post("/school/add", (req, res) => {});
+app.put("/school/update/:id", (req, res) => {});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
