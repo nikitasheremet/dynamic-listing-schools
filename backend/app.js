6c8df187484494a100d044f480c17fbeb6e58064
Nikita Sheremet <nikita.sheremet253@gmail.com>
2019-12-11 21:37:32 -0500

    add body parser dependency to backend to access post data

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 let app = express();
 const port = 4000;
 app.use(cors());
+app.use(bodyParser.json());
 const { Pool } = require("pg");
 
 const pool = new Pool({
@@ -25,7 +27,17 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/school/add", (req, res) => {});
+app.post("/school/add", (req, res) => {
+  let { name, about, location, admission } = req.body;
+  pool
+    .query(
+      `INSERT INTO schools (name, about, location, admission) VALUES ($1, $2, $3, $4)`,
+      [name, about, location, admission]
+    )
+    .then(() => {
+      res.status(200).json({ msg: "success" });
+    });
+});
 app.put("/school/update/:id", (req, res) => {});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
