8cd7eba68c4b9de878357c8d51dcdc194845ff35
Nikita Sheremet <nikita.sheremet253@gmail.com>
2019-12-13 18:25:04 -0500

    Updates

---
+++ precache-manifest.f307ba4c22c7d75400f33d01df2d41ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2f59a534363d3b357c3ffae8ac5850",
+    "url": "/dynamic-listing-schools/master/frontend/index.html"
+  },
+  {
+    "revision": "012221af04eb207c64ce",
+    "url": "/dynamic-listing-schools/master/frontend/static/css/main.9d793321.chunk.css"
+  },
+  {
+    "revision": "fb7a5dfb059b4290c3dd",
+    "url": "/dynamic-listing-schools/master/frontend/static/js/2.8812ae00.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/dynamic-listing-schools/master/frontend/static/js/2.8812ae00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "012221af04eb207c64ce",
+    "url": "/dynamic-listing-schools/master/frontend/static/js/main.d9526bfb.chunk.js"
+  },
+  {
+    "revision": "5f95f4bc36302088e83e",
+    "url": "/dynamic-listing-schools/master/frontend/static/js/runtime-main.159d01bb.js"
+  }
+]);
