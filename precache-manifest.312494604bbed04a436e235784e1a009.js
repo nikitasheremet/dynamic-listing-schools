7bbda711bfa5f5287b17017fec8fc6365ecccffb
Nikita Sheremet <nikita.sheremet253@gmail.com>
2019-12-13 18:22:49 -0500

    Updates

---
+++ precache-manifest.312494604bbed04a436e235784e1a009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052cfd29001338d85a83c5dc337f006e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d575282887d996f5bb83",
+    "url": "/static/css/main.9d793321.chunk.css"
+  },
+  {
+    "revision": "fb7a5dfb059b4290c3dd",
+    "url": "/static/js/2.8812ae00.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.8812ae00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d575282887d996f5bb83",
+    "url": "/static/js/main.bd770739.chunk.js"
+  },
+  {
+    "revision": "810b7eddbba2845a5756",
+    "url": "/static/js/runtime-main.4e64986e.js"
+  }
+]);
